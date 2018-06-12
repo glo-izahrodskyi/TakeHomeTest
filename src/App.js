1b4000bd74e8268b6c71570511b6c63777e8f21c
Ivan Zahrodskyi <ivan.zahrodskyi@globallogic.com>
2018-06-12 20:29:01 +0300

    configure eslint, prettier

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// @flow
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+type Props = {};
+export default class App extends Component<Props> {
+    render() {
+        return <View style={styles.container} />;
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+});
